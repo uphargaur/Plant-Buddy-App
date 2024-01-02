57adf31988293bbcdc54a60ec4f1a6a87800e657
Shaurya33 <shauryamehrotra3@gmail.com>
2024-01-02 14:27:38 +0530

    first quest

---
+++ app/src/main/java/com/taru/example/lost_phone_detection/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lost_phone_detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/taru/example/lost_phone_detection/login_activity.kt
@@ -0,0 +1,11 @@
+package com.example.lost_phone_detection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
